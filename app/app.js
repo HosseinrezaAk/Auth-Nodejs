9d093accf06ba252b0c6c64153fe301613b5f6dc
Hosseinreza <hrakbari77@gmail.com>
2023-02-27 21:51:42 +0330

    Add mongoose

--- app/app.js
+++ app/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
 
 
 app.get("/", function(req, res){
