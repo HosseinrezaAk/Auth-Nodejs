4e7500d9bdbb9d41b6ba6f19b4631b033aaeec47
Hosseinreza <hrakbari77@gmail.com>
2023-03-01 11:59:05 +0330

    Add mongoose-encryption

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
