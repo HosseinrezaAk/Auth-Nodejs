f011f745e9a8b7fa37cf6241519d0a31a79b3b9b
Hosseinreza <hrakbari77@gmail.com>
2023-03-04 20:08:09 +0330

    Comment encryption method

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -19,8 +19,8 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, {secret: secret , encryptedFields: ["password"]})
+// const secret = process.env.SECRET;
+// userSchema.plugin(encrypt, {secret: secret , encryptedFields: ["password"]})
 
 const User = new mongoose.model("User", userSchema);
 
