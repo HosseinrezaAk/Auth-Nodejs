f621e045d363cd5a08b5b67f5e5dd3f9497bbe23
Hosseinreza <hrakbari77@gmail.com>
2023-03-07 11:20:04 +0330

    Delete login and register old version

--- app/app.js
+++ app/app.js
@@ -42,41 +42,12 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req, res){
 
-  bcrypt.hash(req.body.password , saltRounds, function(err, hash) {
-    const newUser = new User({
-      email: req.body.username,
-      password : hash
-    });
-  
-    newUser
-      .save()
-        .then( function(){
-          res.render("secrets");
-        })
-        .catch( function(err){
-          console.log(err);
-        });
-  });
+ 
   
 });
 
 app.post("/login", function(req , res){
-  const username = req.body.username;
-  const password = req.body.password;
-
-
-  User.findOne({email: username})
-    .then( function(foundUser){
-      bcrypt.compare(password, foundUser.password , function(err, result) {
-        if( result === true){
-          res.render("secrets");
-        }
-    });
-      
-    })
-    .catch( function(err){
-      console.log(err);
-    })
+  
 });
 
 
