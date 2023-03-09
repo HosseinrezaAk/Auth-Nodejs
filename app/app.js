e3e30c2ee6fbf18b2875fe79f0ad33357dd4d127
Hosseinreza <hrakbari77@gmail.com>
2023-03-09 11:59:16 +0330

    Add logout route

--- app/app.js
+++ app/app.js
@@ -58,6 +58,17 @@ app.get("/secrets",function(req,res){
   }
 });
 
+app.get("/logout", function(req, res){
+  req.logout( function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.redirect("/");
+    }
+  });
+  
+})
+
 
 
 
