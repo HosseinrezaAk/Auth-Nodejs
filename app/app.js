7f09d8e792c00cd4e4e15946b190ef35bda2d89e
Hosseinreza <hrakbari77@gmail.com>
2023-03-14 21:12:32 +0330

    Add keys

--- app/app.js
+++ app/app.js
@@ -43,7 +43,8 @@ passport.deserializeUser(User.deserializeUser());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://www.example.com/auth/google/callback"
+  callbackURL: "http://localhost:3000/auth/google/secrets"
+  // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
 function(accessToken, refreshToken, profile, cb) {
   User.findOrCreate({ googleId: profile.id }, function (err, user) {
