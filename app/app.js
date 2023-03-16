1b0f4f9eaf18ed3344b4fb73a144c7e225157c16
Hosseinreza <hrakbari77@gmail.com>
2023-03-16 09:29:43 +0330

    Add GET submit route

--- app/app.js
+++ app/app.js
@@ -118,6 +118,16 @@ app.get("/secrets",function(req,res){
   }
 });
 
+app.get("/submit", function( req, res){
+  if(req.isAuthenticated()){
+    res.render("submit");
+  }else{
+    res.redirect("/login");
+  }
+});
+
+
+
 app.get("/logout", function(req, res){
   req.logout( function(err){
     if(err){
